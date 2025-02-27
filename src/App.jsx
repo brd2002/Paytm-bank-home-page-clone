9542b04d95dfd7b00905b4d7dc69b87fdc3ed3eb
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-02-27 23:13:22 +0530

    added mui library

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { NavbarPage } from "./components/NavbarPage";
 import { MainSection } from "./components/MainSection";
+import Button from '@mui/material/Button';
 // import { ReveneuCard } from "./components/RevenueCard";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
+  const [input, setInput] = useState("");
+  console.log(input);
   return (
     <>
-    <NavbarPage/>
-    <MainSection/>
+    {/* <NavbarPage/>
+    <MainSection/> */}
+    <Button>Primary</Button>
+    <TextField id="outlined-basic" onChange={(value)=>{
+      setInput(value.target.value)
+    }
+    } label="Outlined" variant="outlined" />
+    
     </>
   );
 }
--- src/components/NavbarPage.jsx
+++ src/components/NavbarPage.jsx
@@ -2,15 +2,15 @@ import NavbarLogo from "../assets/paytmbank.svg";
 export const NavbarPage = () => {
     return (
         <>
-            <div className="flex bg-white justify-between p-4 shadow-md">
+            <div className="flex w-full bg-white justify-between p-4 shadow-md">
                 <div className="p-4">
                     <img src= {NavbarLogo} alt="logo" className="w-40" />
                 </div>
                 <div className="flex ">
-                    <div className="p-4">Personal Banking</div>
-                    <div className="p-4" >Bussiness Banking</div>
-                    <div className="p-4">Company</div>
-                    <div className="p-4">Support</div>
+                    <div className="p-4 font-bold">Personal Banking</div>
+                    <div className="p-4 font-bold" >Bussiness Banking</div>
+                    <div className="p-4 font-bold">Company</div>
+                    <div className="p-4 font-bold">Support</div>
                 </div>
                 <div className="p-2 ">
                     <button className="px-4 py-2 border-2 border-black text-black bg-white rounded-lg hover:bg-black hover:text-white transition duration-300 mr-4">Download App</button>
