a4c65a34b3d4779432dfec3e5423727a7f55fc25
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-02-27 21:21:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { NavbarPage } from "./components/NavbarPage";
+import { MainSection } from "./components/MainSection";
+// import { ReveneuCard } from "./components/RevenueCard";
+
+function App() {
+  return (
+    <>
+    <NavbarPage/>
+    <MainSection/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,10 @@
+import MainBanner from "../assets/paytmbankBanner.jpg"
+export const MainSection = () =>  { 
+    return(
+        <>
+         <div className="p-10">
+            <img src={MainBanner} alt="MainBanner" />
+         </div>
+        </>
+    )
+}
